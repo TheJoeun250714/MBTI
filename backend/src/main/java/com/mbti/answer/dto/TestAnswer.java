a0760dad3c3bde991d08b847d35e0b0b5608f837
tje0714 <tje0714@outlook.kr>
2025-12-29 15:22:13 +0900

    Initial commit

---
+++ backend/src/main/java/com/mbti/answer/dto/TestAnswer.java
@@ -0,0 +1,13 @@
+package com.mbti.answer.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TestAnswer {
+    private Long questionId;
+    private String selectedOption; // 'A' or 'B'
+}
